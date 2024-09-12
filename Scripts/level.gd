052faa579331a68585281de5196eca4c08c949a7
toiohomaigd <toiohomaigd@gmail.com>
2024-09-13 08:59:16 +1200

    setup scrolling BG and game start area

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
+@onready var start = $Start
 
-@onready var player = $Player
+var player = null
 
 func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	
+	if player != null:
+		player.global_position = start.get_spawn_pos()
+	
 	var traps = get_tree().get_nodes_in_group("traps")
 	var on_trap_touched = Callable(self, "_on_trap_touched_player")
 	
@@ -31,4 +36,4 @@ func _on_trap_touched_player():
 	
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_pos()
---
+++ Scripts/start.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+@onready var spawn_pos = $SpawnPosition
+
+func get_spawn_pos():
+	return spawn_pos.global_position
